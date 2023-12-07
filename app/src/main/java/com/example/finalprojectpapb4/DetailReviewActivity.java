dec74cf22b213b37e11f49a497c5a6c7f96258cc
alihafidz <alihafidz180@gmail.com>
2023-12-07 08:51:57 +0700

    create activity_detail_review.xml and layout for it. then adding style and icon

---
+++ app/src/main/java/com/example/finalprojectpapb4/DetailReviewActivity.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectpapb4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailReviewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_review);
+    }
+}
