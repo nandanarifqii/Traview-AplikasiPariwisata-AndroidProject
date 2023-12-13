7237e9dfa97f245629cc9605d227c29edfd6589f
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2023-12-13 18:59:19 +0700

    feat: navbar

--- app/src/main/java/com/example/finalprojectpapb4/HomeActivity.java
+++ app/src/main/java/com/example/finalprojectpapb4/HomeActivity.java
@@ -1,16 +1,21 @@
 package com.example.finalprojectpapb4;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
     FloatingActionButton button;
     ImageButton searchButton;
+    BottomNavigationView navButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +32,30 @@ public class HomeActivity extends AppCompatActivity {
             Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
             startActivity(intent);
         });
+
+
+        navButton = findViewById(R.id.bottom_navigation);
+
+        navButton.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.beranda) {
+//                    Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+//                    startActivity(homeIntent);
+                    return true;
+
+                } else if (item.getItemId() == R.id.menu_profile) {
+                    Intent profileIntent = new Intent(HomeActivity.this, ProfileActivity.class);
+                    startActivity(profileIntent);
+                    return true;
+
+                }
+                return false;
+                }
+
+
+        }
+        );
     }
+
 }
