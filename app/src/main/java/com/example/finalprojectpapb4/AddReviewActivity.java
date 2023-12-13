d9eaab2ebcf1ccc8da4ab56a0b2a990a4514d4fb
Xrayya <nur.alkhiyar@gmail.com>
2023-12-13 11:42:36 +0700

    feat: connect view component with activity

--- app/src/main/java/com/example/finalprojectpapb4/AddReviewActivity.java
+++ app/src/main/java/com/example/finalprojectpapb4/AddReviewActivity.java
@@ -3,12 +3,30 @@ package com.example.finalprojectpapb4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AddReviewActivity extends AppCompatActivity {
+    private ImageButton backButton;
+    private ImageView reviewImageView;
+    private Button uploadImageButton;
+    private EditText locationEditText;
+    private EditText reviewEditText;
+    private Button postButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
+
+        backButton = findViewById(R.id.icon_back);
+        reviewImageView = findViewById(R.id.review_photo);
+        uploadImageButton = findViewById(R.id.upload_img_button);
+        locationEditText = findViewById(R.id.lokasi_inputfield);
+        reviewEditText = findViewById(R.id.ulasan_inputfield);
+        postButton = findViewById(R.id.btn_submit);
     }
 }
