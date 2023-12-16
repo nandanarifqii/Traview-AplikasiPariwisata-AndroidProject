bd791891d122ed05459027aea98cad845845dfd4
Xrayya <nur.alkhiyar@gmail.com>
2023-12-16 22:50:21 +0700

    feat: update edit profile

--- app/src/main/java/com/example/finalprojectpapb4/EditProfileActivity.java
+++ app/src/main/java/com/example/finalprojectpapb4/EditProfileActivity.java
@@ -12,15 +12,18 @@ import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.util.Date;
 
 public class EditProfileActivity extends AppCompatActivity {
     private ImageView ivImageProfile;
@@ -30,6 +33,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private Button btnSave;
     private Button btnCancel;
     private Uri imageUri;
+    private UserModel currentUser;
     private ActivityResultLauncher<Intent> reviewImageSetter;
 
     @Override
@@ -44,11 +48,37 @@ public class EditProfileActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btn_save);
         btnCancel = findViewById(R.id.btn_cancel);
 
-        Log.d("username", getIntent().getStringExtra("imageUri"));
+        DatabaseReference userReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("user_profiles")
+                .child(FirebaseAuth
+                        .getInstance()
+                        .getCurrentUser()
+                        .getUid());
 
-        etEditUsername.setText(getIntent().getStringExtra("username"));
-        etEditName.setText(getIntent().getStringExtra("name"));
-//        ivImageProfile.setImageURI(Uri.parse(getIntent().getStringExtra("imageUri")));
+        userReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                currentUser = snapshot.getValue(UserModel.class);
+                etEditName.setText(currentUser.getName());
+                etEditUsername.setText(currentUser.getUsername());
+                if (currentUser.getImageProfileUri() != null) {
+                    Glide.with(getApplicationContext())
+                            .load(currentUser.getImageProfileUri())
+                            .into(ivImageProfile);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Error occurred: " + error.getMessage(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
 
         reviewImageSetter = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -68,11 +98,17 @@ public class EditProfileActivity extends AppCompatActivity {
         );
 
         btnUploadProfile.setOnClickListener(_view -> pickImage());
-        btnSave.setOnClickListener(_view -> updateProfile());
+        btnSave.setOnClickListener(_view -> updateProfile(
+                etEditUsername.getText().toString(),
+                etEditName.getText().toString()
+        ));
         btnCancel.setOnClickListener(_view -> backToProfile());
     }
 
-    private void updateProfile() {
+    private void updateProfile(String username, String name) {
+        if (!validateForm(username, name)) {
+            return;
+        }
         DatabaseReference reviewReference = FirebaseDatabase
                 .getInstance()
                 .getReference()
@@ -98,10 +134,10 @@ public class EditProfileActivity extends AppCompatActivity {
                     ).show();
                 });
 
-        UserModel updatedProfile = new UserModel(
-                etEditUsername.getText().toString(),
-                etEditName.getText().toString());
-        updatedProfile.setImageProfileUri(imageUri.toString());
+        UserModel updatedProfile = new UserModel(username, name);
+        updatedProfile.setImageProfileUri(imageUri == null ?
+                currentUser.getImageProfileUri() :
+                imageUri.toString());
 
         reviewReference.setValue(updatedProfile, (error, ref) -> {
             if (error != null) {
@@ -145,6 +181,12 @@ public class EditProfileActivity extends AppCompatActivity {
                     "Image can not be empty",
                     Toast.LENGTH_SHORT
             ).show();
+            return false;
+        }
+
+        if (username.equals(currentUser.getUsername()) &&
+                name.equals(currentUser.getName()) &&
+                (imageUri.toString().equals(currentUser.getImageProfileUri()) || imageUri == null)) {
             return false;
         }
 
